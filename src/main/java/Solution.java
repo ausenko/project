1b2dca6c412d45a3b6eb9a7a3152246dc68f1970
Anton <anton.usenko87@gmail.com>
2015-12-19 00:09:06 +0200

    isPrimeNew

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -6,28 +6,25 @@ import java.math.BigInteger;
 public class Solution {
 
 
-    public int primeNumber(int endvalue) {
-
-        boolean secondtype = false;
-
-
-        for (int i = 2; i <= endvalue; i++) {
-            for (int j = 2; j < i; j++) {
-                secondtype = (i % j == 0);
-                if (secondtype) break;
-            }
-            if (!secondtype) {
-                System.out.print(i + ", ");
-
+    public static boolean isPrime(int endvalue) {
+        if (endvalue == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= endvalue; i++) {
+            if (endvalue % i == 0) {
+                return false;
             }
         }
-        return endvalue;
+        return true;
     }
 
 
     public static void main(String[] args) {
-        Solution sl = new Solution();
-        sl.primeNumber(25);
+
+        System.out.println();
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) System.out.print(i + ", ");
+        }
     }
 
 
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,5 +1,9 @@
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by zhenya on 16.12.15.
  */
@@ -7,14 +11,15 @@ public class SolutionTest {
 
 
     @Test
-    public void testPrimeNumber(){
-        Solution sl = new Solution();
+    public void testIsPrime() {
+        assertFalse(Solution.isPrime(1));
+        assertTrue(Solution.isPrime(2));
+        assertTrue(Solution.isPrime(13));
+        assertTrue(Solution.isPrime(23));
+        assertFalse(Solution.isPrime(62));
 
-   //   assertEquals(,sl.primeNumber(10));
 
     }
-
-
 
 
 }

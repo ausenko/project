479d82f11a72adba4e38eb21dd7e7f4f3a83f903
Anton <anton.usenko87@gmail.com>
2016-02-22 22:56:12 +0200

    Linked List Cycle

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Anton on 22.02.16.
+ */
+public class SolutionTest {
+
+    ListNode listNode  = new ListNode();
+
+    @Test
+    public void testhachCycle(){
+        assertEquals( ListNode.ha)
+    }
+}
